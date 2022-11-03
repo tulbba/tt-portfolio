4897293e6eeb9d9a08f3f25024fd6ae0ffe4137f
Tuuli <tuuli.torkkeli@gmail.com>
2022-11-03 12:44:13 +0100

    fixes to header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Work() {
     return (
-        <div className="sm:mix-blend-exclusion container w-full fixed top-0 z-10 p-1">   
-            <ul className="text-white bg-black sm:bg-black/0 flex flex-row justify-center gap-8 sm:justify-end sm:gap-14 font-body uppercase text-lg">
+        <div className="sm:mix-blend-exclusion container w-full fixed top-0 z-10">   
+            <ul className="text-white p-1 bg-black sm:bg-black/0 flex flex-row justify-center gap-8 sm:justify-end sm:gap-14 font-body uppercase text-lg">
                 <li>
                     <a href="#project">Projects</a>
                 </li>
